653e068f3db0d69ecebd92b86713ca40f972d994
liik <linyuanqianpp@126.com>
2017-06-19 16:22:25 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+var uglify = require('gulp-uglify');
+var minicss = require('gulp-minify-css')
+var browserSync = require('browser-sync').create();
+// var clean = require('gulp-clean')
+var reload = browserSync.reload;
+
+// 处理sass文件
+gulp.task('sass', function() {
+    gulp.src('dev/scss/*.scss')
+        .pipe(sass())
+        .pipe(minicss())
+        .pipe(gulp.dest('build/css'))
+})
+
+// 静态服务器
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
+gulp.task('jsmin', function() {
+    gulp.src('dev/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('movehtml', function() {
+    gulp.src('dev/index.html')
+        .pipe(gulp.dest('build'))
+})
+
+// watch任务
+gulp.task('watch', ['sass', 'jsmin'], function() {
+    browserSync.init({
+        server: "./build"
+    });
+    gulp.watch("dev/scss/*.scss", ['sass']);
+    gulp.watch("dev/js/*.js", ['jsmin']);
+    gulp.watch("dev/*.html", ['movehtml']).on('change', reload);
+})
+
+gulp.task('build', function() {
+
+})
